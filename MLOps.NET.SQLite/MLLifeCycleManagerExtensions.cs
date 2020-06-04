0666e366aa2b138acdb7998b6ba57008cac70631
Daniel Costea <daniel_costea@ymail.com>
2020-06-04 14:59:14 +0200

    Add UseSQLite

---
+++ MLOps.NET.SQLite/MLLifeCycleManagerExtensions.cs
@@ -0,0 +1,14 @@
+﻿using MLOps.NET.Storage;
+
+namespace MLOps.NET.SQLite
+{
+    public static class MLLifeCycleManagerExtensions
+    {
+        public static MLLifeCycleManager UseSQLite(this MLLifeCycleManager mLLifeCycleManager)
+        {
+            mLLifeCycleManager.MetaDataStore = new SQLiteMetaDataStore();
+
+            return mLLifeCycleManager;
+        }
+    }
+}
