21dc19af111f8366764fbfb45dc1c2d13b17fc5a
Alexander Slotte <alexander.slotte@gmail.com>
2020-06-09 16:41:16 -0400

    Added console output

--- examples/MLOps.NET.Examples/MLOps.NET.Examples/Program.cs
+++ examples/MLOps.NET.Examples/MLOps.NET.Examples/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.ML;
 using MLOps.NET.MulticlassClassification.Entities;
 using MLOps.NET.SQLite;
+using System;
 using System.Threading.Tasks;
 
 namespace MLOps.NET.MulticlassClassification
@@ -10,6 +11,7 @@ namespace MLOps.NET.MulticlassClassification
         static async Task Main(string[] args)
         {
             // MLOps: Create experiment and run
+            Console.WriteLine("Creating an MLOps Experiment and Run");
             var mlOpsContext = new MLOpsBuilder()
                 .UseSQLite(@"C:/MLOps")
                 .Build();
@@ -18,11 +20,11 @@ namespace MLOps.NET.MulticlassClassification
 
             var mlContext = new MLContext(seed: 1);
 
-            //Load data
+            Console.WriteLine("Loading the data");
             var data = mlContext.Data.LoadFromTextFile<ProductInformation>("Data/ecommerce.csv", hasHeader: true, separatorChar: ',');
             var testTrainTest = mlContext.Data.TrainTestSplit(data);
 
-            //Transform
+            Console.WriteLine("Creating a data processing pipeline");
             var dataProcessingPipeline = mlContext.Transforms.Conversion.MapValueToKey(nameof(ProductInformation.Category))
                 .Append(mlContext.Transforms.Text.FeaturizeText(nameof(ProductInformation.ProductName)))
                 .Append(mlContext.Transforms.Text.FeaturizeText(nameof(ProductInformation.Description)))
@@ -30,24 +32,26 @@ namespace MLOps.NET.MulticlassClassification
                 .Append(mlContext.Transforms.Concatenate("Features", nameof(ProductInformation.ProductName), nameof(ProductInformation.Description), nameof(ProductInformation.Brand), nameof(ProductInformation.Price))
                 .Append(mlContext.Transforms.NormalizeMinMax("Features"))));
 
-            //Train the model
+            Console.WriteLine("Training the model, please stand-by...");
             var trainingPipeline = dataProcessingPipeline
                 .Append(mlContext.MulticlassClassification.Trainers.SdcaMaximumEntropy(nameof(ProductInformation.Category), "Features")
                 .Append(mlContext.Transforms.Conversion.MapKeyToValue("PredictedLabel")));
 
             var trainedModel = trainingPipeline.Fit(testTrainTest.TrainSet);
 
-            //Evaluate the model
+            Console.WriteLine("Evaluating the model");
             var predictions = trainedModel.Transform(testTrainTest.TestSet);
             var metrics = mlContext.MulticlassClassification.Evaluate(predictions, nameof(ProductInformation.Category));
 
             //MLOps: Log Metrics
+            Console.WriteLine("Logging metrics");
             await mlOpsContext.LogMetricsAsync(runId, metrics);
 
             //Save the model
             mlContext.Model.Save(trainedModel, testTrainTest.TrainSet.Schema, "MultiClassificationModel.zip");
 
             //MLOps: Upload artifact/model
+            Console.WriteLine("Uploading artifact");
             await mlOpsContext.UploadModelAsync(runId, "MultiClassificationModel.zip");
         }
     }
