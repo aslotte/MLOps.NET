7d43038826080fc45fccbf13b19f1356f5d4e56f
Daniel Costea <daniel_costea@ymail.com>
2020-06-17 21:25:31 +0200

    Add regression example. #19

---
+++ examples/MLOps.NET.Examples/MLOps.NET.Regression/Entities/ModelInput.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.ML.Data;
+
+namespace MLOps.NET.Regression.Entities
+{
+    class ModelInput
+    {
+        [LoadColumn(0)]
+        public string VendorId { get; set; }
+
+        [LoadColumn(1)]
+        public string RateCode { get; set; }
+
+        [LoadColumn(2)]
+        public float PassengerCount { get; set; }
+
+        [LoadColumn(3)]
+        public float TripTime { get; set; }
+
+        [LoadColumn(4)]
+        public float TripDistance { get; set; }
+
+        [LoadColumn(5)]
+        public string PaymentType { get; set; }
+
+        [LoadColumn(6)]
+        public float FareAmount { get; set; }
+    }
+}
---
+++ examples/MLOps.NET.Examples/MLOps.NET.Regression/Entities/ModelOutput.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.ML.Data;
+
+namespace MLOps.NET.Regression.Entities
+{
+    public class ModelOutput
+    {
+        [ColumnName("Score")]
+        public float FareAmount;
+    }
+}
