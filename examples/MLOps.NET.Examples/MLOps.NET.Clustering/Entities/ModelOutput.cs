cc8c36aec27aea3efe065c84dbbcda1bc9ff19c2
Brett Parker <67550533+Brett-Parker@users.noreply.github.com>
2020-07-06 21:04:00 +0100

    Revert "Merge branch 'Issue_182' of https://github.com/Brett-Parker/MLOps.NET into Issue_182"
    This reverts commit 175db8a3a509474c1972f6795449e6ebf16ef6e0, reversing
    changes made to 2839fc7cab4e8319752121343dac0ecce5bbbc1b.

---
+++ examples/MLOps.NET.Examples/MLOps.NET.Clustering/Entities/ModelOutput.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MLOps.NET.Clustering.Entities
+{
+    class ModelOutput
+    {
+        // Original label (not used during training, just for comparison).
+        public uint Label { get; set; }
+        // Predicted label from the trainer.
+        public uint PredictedLabel { get; set; }
+    }
+}
