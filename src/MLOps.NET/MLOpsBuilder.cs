5be5d76ae303070a5c435f49a19527083e7656d0
Brett Parker <67550533+Brett-Parker@users.noreply.github.com>
2020-07-06 16:57:19 +0100

    Update MLOpsBuilder.cs

--- src/MLOps.NET/MLOpsBuilder.cs
+++ src/MLOps.NET/MLOpsBuilder.cs
@@ -8,42 +8,6 @@ namespace MLOps.NET
     /// </summary>
     public class MLOpsBuilder
     {
-        private IMetaDataStore metaDataStore;
-        private IModelRepository modelRepository;
-
-        /// <summary>
-        /// Build the <see cref="IMLOpsContext"/> using the provided configuration
-        /// </summary>
-        /// <returns>Configured <see cref="IMLOpsContext"/></returns>
-        public IMLOpsContext Build()
-        {
-            return new MLOpsContext(metaDataStore, modelRepository);
-        }
-
-        /// <summary>
-        /// Configures the builder to use the provided <see cref="IMetaDataStore"/>
-        /// </summary>
-        /// <param name="metaDataStore"><see cref="IMetaDataStore"/> to use in the MLOps pipeline</param>
-        /// <returns>The current builder for chaining</returns>
-        public MLOpsBuilder UseMetaDataStore(IMetaDataStore metaDataStore)
-        {
-            if (this.metaDataStore != null) throw new InvalidOperationException("MetaDataStore is already configured");
-
-            this.metaDataStore = metaDataStore ?? throw new ArgumentNullException(nameof(metaDataStore));
-            return this;
-        }
-
-        /// <summary>
-        /// Configures the builder to use the provided <see cref="IModelRepository"/>
-        /// </summary>
-        /// <param name="modelRepository"><see cref="IModelRepository"/> to use in the MLOps pipeline</param>
-        /// <returns>The current builder for chaining</returns>
-        public MLOpsBuilder UseModelRepository(IModelRepository modelRepository)
-        {
-            if (this.modelRepository != null) throw new InvalidOperationException("ModelRepository is already configured");
-
-            this.modelRepository = modelRepository ?? throw new ArgumentNullException(nameof(modelRepository));
-            return this;
-        }
+        
     }
 }
