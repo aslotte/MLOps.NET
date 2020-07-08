1a807d3dd45c9c9d72390a778033e9894e2fc391
Alexander Slotte <alexander.slotte@gmail.com>
2020-07-08 06:53:13 -0400

    Remove unit tests that don't add value

--- test/MLOps.NET.Azure.Tests/MLOpsBuilderExtensionTests.cs
+++ test/MLOps.NET.Azure.Tests/MLOpsBuilderExtensionTests.cs
@@ -24,54 +24,5 @@ namespace MLOps.NET.Azure.Tests
             //Assert
             unitUnderTest.Evaluation.Should().NotBeNull();
         }
-
-        
-        [TestMethod]
-        public void UseAzureStorage_ConfiguresTrainingCatalog()
-        {
-            //Act
-            IMLOpsContext unitUnderTest = new MLOpsBuilder()
-                .UseAzureBlobModelRepository("UseDevelopmentStorage=true")
-                .UseCosmosDb("https://localhost:8081", "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==")
-                .Build();
-
-            unitUnderTest.Should().BeOfType<MLOpsContext>("Because the default IMLLifeCycleManager is MLLifeCycleManager");
-
-            //Assert
-            unitUnderTest.Training.Should().NotBeNull();
-        }
-
-        [TestMethod]
-        public void UseAzureStorage_ConfiguresLifeCycleCatalog()
-        {
-            //Act
-            IMLOpsContext unitUnderTest = new MLOpsBuilder()
-                .UseAzureBlobModelRepository("UseDevelopmentStorage=true")
-                .UseCosmosDb("https://localhost:8081", "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==")
-                .Build();
-
-            unitUnderTest.Should().BeOfType<MLOpsContext>("Because the default IMLLifeCycleManager is MLLifeCycleManager");
-
-            //Assert
-            unitUnderTest.LifeCycle.Should().NotBeNull();
-        }
-
-        [TestMethod]
-        public void UseAzureStorage_ConfiguresModelCatalog()
-        {
-            //Act
-            IMLOpsContext unitUnderTest = new MLOpsBuilder()
-                .UseAzureBlobModelRepository("UseDevelopmentStorage=true")
-                .UseCosmosDb("https://localhost:8081", "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==")
-                .Build();
-
-            unitUnderTest.Should().BeOfType<MLOpsContext>("Because the default IMLLifeCycleManager is MLLifeCycleManager");
-
-            //Assert
-            unitUnderTest.Model.Should().NotBeNull();
-
-            var repositoryField = typeof(ModelCatalog).GetField("modelRepository", BindingFlags.Instance | BindingFlags.NonPublic);
-            repositoryField.GetValue(unitUnderTest.Model).Should().BeOfType<StorageAccountModelRepository>();
-        }
     }
 }
