ee7ecdc1d327d3775594e963d7233b80db18ae85
Brett Parker <67550533+Brett-Parker@users.noreply.github.com>
2020-07-17 22:41:01 +0100

    Create SQLServer Unit Test
    Took the existing MLOpsBuilderExtensionTests.cs that was stored in the integration tests and added a new SQLServer unit test project to align with the other storage tests.

---
+++ test/MLOps.NET.SQLServer.Tests/MLOpsBuilderExtensionTests.cs
@@ -0,0 +1,60 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MLOps.NET.Storage;
+using Moq;
+
+namespace MLOps.NET.SQLServer.Tests
+{
+    [TestCategory("UnitTests")]
+    [TestClass]
+    public class MLOpsBuilderExtensionTests
+    {
+        private const string connectionString = "Server=localhost,1433;Database=MLOpsNET_IntegrationTests;User Id=sa;Password=MLOps4TheWin!;";
+
+        [TestMethod]
+        public void UseSQLServerStorage_ConfiguresEvaluationCatalog()
+        {
+            //Act
+            IMLOpsContext unitUnderTest = new MLOpsBuilder()
+                .UseSQLServer(connectionString)
+                .UseModelRepository(new Mock<IModelRepository>().Object)
+                .Build();
+
+            unitUnderTest.Should().BeOfType<MLOpsContext>("Because the default IMLLifeCycleManager is MLLifeCycleManager");
+
+            //Assert
+            unitUnderTest.Evaluation.Should().NotBeNull();
+        }
+
+
+        [TestMethod]
+        public void UseSQLServerStorage_ConfiguresTrainingCatalog()
+        {
+            //Act
+            IMLOpsContext unitUnderTest = new MLOpsBuilder()
+                .UseSQLServer(connectionString)
+                .UseModelRepository(new Mock<IModelRepository>().Object)
+                .Build();
+
+            unitUnderTest.Should().BeOfType<MLOpsContext>("Because the default IMLLifeCycleManager is MLLifeCycleManager");
+
+            //Assert
+            unitUnderTest.Training.Should().NotBeNull();
+        }
+
+        [TestMethod]
+        public void UseSQLServer_ConfiguresLifeCycleCatalog()
+        {
+            //Act
+            IMLOpsContext unitUnderTest = new MLOpsBuilder()
+                .UseSQLServer(connectionString)
+                .UseModelRepository(new Mock<IModelRepository>().Object)
+                .Build();
+
+            unitUnderTest.Should().BeOfType<MLOpsContext>("Because the default IMLLifeCycleManager is MLLifeCycleManager");
+
+            //Assert
+            unitUnderTest.LifeCycle.Should().NotBeNull();
+        }
+    }
+}
