2a6a67b5c105c124f13377d50e84978d677c440c
Alexander Slotte <alexander.slotte@gmail.com>
2020-07-25 13:06:50 -0400

    Added RunRepository unit tests

---
+++ test/MLOps.NET.Tests/Repositories/RunRepositoryTests.cs
@@ -0,0 +1,77 @@
+﻿using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MLOps.NET.Entities.Impl;
+using MLOps.NET.Storage;
+using MLOps.NET.Storage.Database;
+using MLOps.NET.Storage.EntityConfiguration;
+using MLOps.NET.Utilities;
+using Moq;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MLOps.NET.Tests
+{
+    [TestCategory("UnitTests")]
+    [TestClass]
+    public class ModelCatalogTests
+    {
+        private Mock<IClock> clockMock;
+        private DbContextFactory contextFactory;
+        private RunRepository sut;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            var options = new DbContextOptionsBuilder<MLOpsDbContext>()
+                .UseInMemoryDatabase(databaseName: "MLOpsNET")
+                .Options;
+
+            this.contextFactory = new DbContextFactory(options, RelationalEntityConfigurator.OnModelCreating);
+
+            this.clockMock = new Mock<IClock>();
+
+            this.sut = new RunRepository(contextFactory, clockMock.Object);
+        }
+
+        [TestMethod]
+        public async Task RegisterModel_ShouldSetRegisteredDate()
+        {
+            //Arrange
+            var (ExperimentId, RunId) = await SetupSeedDate();
+
+            var now = DateTime.Now;
+            this.clockMock.Setup(x => x.UtcNow).Returns(now);
+
+            //Act
+            await this.sut.CreateRegisteredModelAsync(ExperimentId, RunId, "By me");
+
+            //Assert
+            using var db = this.contextFactory.CreateDbContext();
+            var registeredModel = db.RegisteredModels.First();
+
+            registeredModel.RegisteredDate.Should().Be(now);
+        }
+
+        private async Task<(Guid ExperimentId, Guid RunId)> SetupSeedDate()
+        {
+            using var db = contextFactory.CreateDbContext();
+
+            var experiment = new Experiment("Test");
+            var run = new Run(experiment.ExperimentId);
+            var runArtifact = new RunArtifact()
+            {
+                RunId = run.RunId
+            };
+
+            db.Experiments.Add(experiment);
+            db.Runs.Add(run);
+            db.RunArtifacts.Add(runArtifact);
+
+            await db.SaveChangesAsync();
+
+            return (experiment.ExperimentId, runArtifact.RunArtifactId);
+        }
+    }
+}
