a1702121f41462a054bd2db5ec1edb6e034327ed
Alexander Slotte <alexander.slotte@gmail.com>
2020-07-25 09:49:18 -0400

    Broke configuration out for SQLServer tests

---
+++ test/MLOps.NET.SQLServer.IntegrationTests/ConfigurationKeys.cs
@@ -0,0 +1,7 @@
+﻿namespace MLOps.NET.SQLServer.IntegrationTests.Constants
+{
+    internal class ConfigurationKeys
+    {
+        public static string ConnectionString = "SQLServer:ConnectionString";
+    }
+}
