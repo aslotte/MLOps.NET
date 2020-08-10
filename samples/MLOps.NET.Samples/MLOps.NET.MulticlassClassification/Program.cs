4c9b22e536bf81adab00e0998d01d7e3754f5c26
Alexander Slotte <alexander.slotte@gmail.com>
2020-08-09 20:57:22 -0400

    Updates to use v1.0.0

--- samples/MLOps.NET.Samples/MLOps.NET.MulticlassClassification/Program.cs
+++ samples/MLOps.NET.Samples/MLOps.NET.MulticlassClassification/Program.cs
@@ -1,10 +1,12 @@
 ﻿using Microsoft.ML;
 using Microsoft.ML.Trainers;
+using MLOps.NET.Extensions;
 using MLOps.NET.MulticlassClassification.Entities;
 using MLOps.NET.SQLite;
 using MLOps.NET.Storage;
 using System;
 using System.Diagnostics;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace MLOps.NET.MulticlassClassification
@@ -18,10 +20,12 @@ namespace MLOps.NET.MulticlassClassification
             // MLOps: Create experiment and run
             var mlOpsContext = new MLOpsBuilder()
                 .UseSQLite()
+                .UseLocalFileModelRepository()
                 .Build();
 
             Console.WriteLine("Creating an MLOps Run");
-            var runId = await mlOpsContext.LifeCycle.CreateRunAsync("Product Category Predictor");
+            var experimentId = await mlOpsContext.LifeCycle.CreateExperimentAsync("Product Category Predictor");
+            var runId = await mlOpsContext.LifeCycle.CreateRunAsync(experimentId);
             Console.WriteLine($"Run created with Id {runId}");
 
             var mlContext = new MLContext(seed: 1);
@@ -29,6 +33,9 @@ namespace MLOps.NET.MulticlassClassification
             Console.WriteLine("Loading the data");
             var data = mlContext.Data.LoadFromTextFile<ProductInformation>("Data/ecommerce.csv", hasHeader: true, separatorChar: ',');
             var testTrainTest = mlContext.Data.TrainTestSplit(data);
+
+            //MLOps: Log data - e.g. schema, columns, types and a hash to track changes
+            await mlOpsContext.Data.LogDataAsync(runId, data);
 
             Console.WriteLine("Creating a data processing pipeline");
             var dataProcessingPipeline = mlContext.Transforms.Conversion.MapValueToKey(nameof(ProductInformation.Category))
@@ -47,7 +54,7 @@ namespace MLOps.NET.MulticlassClassification
             
             var trainedModel = trainingPipeline.Fit(testTrainTest.TrainSet);
 
-            await mlOpsContext.Training.LogHyperParametersAsync<SdcaMaximumEntropyMulticlassTrainer>(runId, trainer);
+            await mlOpsContext.Training.LogHyperParametersAsync(runId, trainer);
             stopwatch.Stop();
 
             //MLOps: Training time
@@ -69,6 +76,23 @@ namespace MLOps.NET.MulticlassClassification
             //MLOps: Upload artifact/model
             Console.WriteLine("Uploading artifact");
             await mlOpsContext.Model.UploadAsync(runId, "MultiClassificationModel.zip");
+
+            //MLOps: Optional - Register model
+            Console.WriteLine("Registering model");
+            var runArtifact = mlOpsContext.Model.GetRunArtifacts(runId).First();
+            await mlOpsContext.Model.RegisterModel(experimentId, runArtifact.RunArtifactId, "John Doe");
+            var registeredModel = mlOpsContext.Model.GetLatestRegisteredModel(experimentId);
+
+            //MLOps: Optional - Create deployment target
+            Console.WriteLine("Creating a deployment target");
+            await mlOpsContext.Deployment.CreateDeploymentTargetAsync("Test");
+            var deploymentTarget = mlOpsContext.Deployment.GetDeploymentTargets().First(x => x.Name == "Test");
+
+            //MLOps: Optional - Deploy model
+            Console.WriteLine("Deploying the model");
+            var deploymentUri = await mlOpsContext.Deployment.DeployModelAsync(deploymentTarget, registeredModel, deployedBy: "John Doe");
+
+            Console.WriteLine($"Model deployed to: {deploymentUri}");
         }
     }
 }
