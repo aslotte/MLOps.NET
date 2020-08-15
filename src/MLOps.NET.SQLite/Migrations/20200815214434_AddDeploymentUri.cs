727a011f91d5fba539146c95049bdea4deff1561
Alexander Slotte <alexander.slotte@gmail.com>
2020-08-15 17:58:07 -0400

    Added SQLite migration

---
+++ src/MLOps.NET.SQLite/Migrations/20200815214434_AddDeploymentUri.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MLOps.NET.SQLite.Migrations
+{
+    public partial class AddDeploymentUri : Migration
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="migrationBuilder"></param>
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "DeploymentUri",
+                table: "Deployment",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="migrationBuilder"></param>
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DeploymentUri",
+                table: "Deployment");
+        }
+    }
+}
