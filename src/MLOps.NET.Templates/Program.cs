e7dcffa51973c3e3d6c10841bde0a39c2e57bafc
Alexander Slotte <alexander.slotte@gmail.com>
2020-08-22 13:43:08 -0400

    Renamed template project and created a template package project

---
+++ src/MLOps.NET.Templates/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MLOps.NET.Templates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
