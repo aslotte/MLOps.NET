5d542a285e2ba76f7aa93f496b237c7ddf57d15e
Brett Parker <67550533+Brett-Parker@users.noreply.github.com>
2020-08-29 22:53:05 +0100

    Baseline commit

---
+++ Benchmarks/MLOps.NET.Benchmarks/Benchmark.cs
@@ -0,0 +1,13 @@
+﻿using BenchmarkDotNet.Running;
+using System;
+
+namespace MLOps.NET.Benchmarks
+{
+    public class Benchmark
+    {
+        public static void Main(string[] args)
+        {
+            var lifeCylceCatalogBenchmarks = BenchmarkRunner.Run<LifeCylceCatalogBenchmarks>();
+        }
+    }
+}
