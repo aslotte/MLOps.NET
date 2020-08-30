e03e38dbc7b34c63f3106699cc8602779763884a
Brett Parker <67550533+Brett-Parker@users.noreply.github.com>
2020-08-30 13:52:00 +0100

    Added project folder structure

---
+++ Benchmarks/MLOps.NET.Benchmarks/SQLLite/BenchmarkSetup.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MLOps.NET.Extensions;
+using MLOps.NET.SQLite;
+using MLOps.NET.Storage.Database;
+using MLOps.NET.Storage.Interfaces;
+
+namespace MLOps.NET.Benchmarks
+{
+    internal class BenchmarkSetup
+    {
+        internal static IMLOpsContext Initialize()
+        {
+            return new MLOpsBuilder()
+                .UseLocalFileModelRepository()
+                .UseSQLite()
+                .Build();
+        }
+    }
+}
---
+++ Benchmarks/MLOps.NET.Benchmarks/SQLLite/LifeCylceCatalogBenchmark.cs
@@ -0,0 +1,24 @@
+﻿using BenchmarkDotNet.Attributes;
+using MLOps.NET.Extensions;
+using MLOps.NET.SQLite;
+using System.Threading.Tasks;
+
+namespace MLOps.NET.Benchmarks
+{
+    public class LifeCylceCatalogBenchmarks
+    {
+        private IMLOpsContext sut;
+
+        [GlobalSetup]
+        public void Initialize()
+        {
+            sut = BenchmarkSetup.Initialize();
+        }
+
+        [Benchmark]
+        public async Task CreateRunAsync()
+        {
+            await sut.LifeCycle.CreateRunAsync("Benchmark");
+        }
+    }
+}
