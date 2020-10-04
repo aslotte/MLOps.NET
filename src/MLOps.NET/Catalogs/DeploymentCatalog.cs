2b77096d65dfd73aaec0be8f2ad6700998fb48e6
Alexander Slotte <alexander.slotte@gmail.com>
2020-10-04 13:43:02 -0400

    Removed namespaces

--- src/MLOps.NET/Catalogs/DeploymentCatalog.cs
+++ src/MLOps.NET/Catalogs/DeploymentCatalog.cs
@@ -11,7 +11,6 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 
 namespace MLOps.NET.Catalogs
